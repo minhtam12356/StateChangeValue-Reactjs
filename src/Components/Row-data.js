78fbb8f1fdfed87f79e332984a955df5a8069a0b
minhtam12356 <tam26918@gmail.com>
2021-02-15 22:40:59 +0700

    .

--- src/Components/Row-data.js
+++ src/Components/Row-data.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 class RowData extends React.Component{
-    render(){
+    render(){  
+        let className = 'img';
         const click = () => {
             var getTr = document.getElementsByTagName('tr');
             var numberGetTr = parseInt(this._reactInternals.return.key) + 1;
             var numberGetImg = this._reactInternals.key;
             var getChoice = getTr[numberGetTr].children[numberGetImg];
             getChoice.children[0].setAttribute('width', parseInt(getChoice.children[0].width) + 10)
-        }
+        };
         return (  
             <td>
-                <img src={this.props.src} alt="Image 1" width={this.props.width} onClick={click}/>
+                <img className={className} src={this.props.src} alt="Image 1" width={this.props.width} onClick={click}/>
             </td> 
         )}       
     
